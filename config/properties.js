64d568dbeb02e9dc4ac1a22fa6d79dd83b0016ec
Sj1218305 <jainshobit0@gmail.com>
2020-06-04 01:26:09 +0530

    added mongoDB client

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT : 4000,
-    DB : 'mongodb://localhost:27017/test',
+    DB : 'mongodb+srv://Shobhit:shobhit@hello-rgvtw.mongodb.net/test?retryWrites=true&w=majority',
 }
